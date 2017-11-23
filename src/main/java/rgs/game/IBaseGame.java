52ac9d0cc9aa2b89bbc1b9e99aba8e9042aecf98
Felix Ding <uraplutonium@yandex.com>
2017-11-23 11:04:06 +0800

    First commit

---
+++ src/main/java/rgs/game/IBaseGame.java
@@ -0,0 +1,5 @@
+package rgs.game;
+
+public interface IBaseGame {
+	void test();
+}
---
+++ src/main/java/rgs/sandbox/Sandbox.java
@@ -0,0 +1,11 @@
+package rgs.sandbox;
+
+public class Sandbox {
+
+	public static void main(String[] args) {
+		
+		System.out.println("sandbox start running.");
+
+	}
+
+}
